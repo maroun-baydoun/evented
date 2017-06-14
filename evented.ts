e9ef6256485269ec7142e621fe816deeefcf73e5
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-06-14 23:25:09 +0900

    Improved code style

--- evented.ts
+++ evented.ts
@@ -1,168 +1,163 @@
 
 export class Evented {
 
-    private listeners: { [eventName: string]: Array<Listener>};
-    private static globalListeners: { [eventName: string]: Array<Listener>} = {};
+  private listeners: ListenerDictionary;
+  private static globalListeners: ListenerDictionary = {};
 
-    constructor() {
+  constructor() {
 
-        this.listeners = {};
+    this.listeners = {};
+  }
+
+  on<T>(eventName: string, listener: Listener<T>): () => void {
+
+    return Evented._on<T>(this, eventName, listener);
+
+  }
+
+  off(eventName: string, listener: Listener): void {
+
+    Evented._off(this, eventName, listener);
+  }
+
+  fire<T>(eventName: string, eventArgs?: T, eventTarget: Object = this): void {
+
+    Evented._fire<T>(this, eventName, eventArgs, eventTarget);
+  }
+
+  listensTo(eventName: string): boolean {
+
+    return Evented._listensTo(this, eventName);
+  }
+
+  static fire<T>(eventName: string, eventArgs?: T, eventTarget?: Object): void {
+
+    Evented._fire<T>(undefined, eventName, eventArgs, eventTarget);
+  }
+
+  static on<T>(eventName: string, listener: Listener<T>): () => void {
+
+    return Evented._on<T>(undefined, eventName, listener);
+  }
+
+  static off(eventName: string, listener: Listener): void {
+
+    Evented._off(undefined, eventName, listener);
+  }
+
+  static listensTo(eventName: string): boolean {
+
+    return Evented._listensTo(undefined, eventName);
+  }
+
+  private static _on<T>(instance: Evented | undefined, eventName: string, listener: Listener<T>): () => void {
+
+    let listeners: ListenerDictionary;
+
+    if (instance) {
+      listeners = instance.listeners;
+    } else {
+      listeners = Evented.globalListeners;
     }
 
-    on<T>(eventName: string, listener: Listener<T>): () => void {
-
-        return Evented._on<T>(this, eventName, listener);
-
+    if (listeners[eventName] === undefined) {
+      listeners[eventName] = [];
     }
 
-    off(eventName: string, listener: Listener): void {
+    listeners[eventName].push(listener);
 
-        Evented._off(this, eventName, listener);
+    return () => {
+      Evented._off(instance, eventName, listener);
+    };
+
+  }
+
+  private static _off(instance: Evented | undefined, eventName: string, listener: Listener): void {
+
+    let eventListeners: Listener[],
+      i: number,
+      length: number,
+      listeners: ListenerDictionary;
+
+    if (instance) {
+      listeners = instance.listeners;
+    } else {
+      listeners = Evented.globalListeners;
     }
 
-    fire<T>(eventName: string, eventArgs?: T, eventTarget: Object = this): void {
-
-        Evented._fire<T>(this, eventName, eventArgs, eventTarget);
-    }
-
-    listensTo(eventName: string): boolean {
-
-        return Evented._listensTo(this, eventName);
-    }
-
-    static fire<T>(eventName: string, eventArgs?: T, eventTarget?: Object ): void {
-
-        Evented._fire<T>(undefined, eventName, eventArgs, eventTarget);
-    }
-
-    static on<T>(eventName: string, listener: Listener<T>): () => void {
-
-        return Evented._on<T>(undefined, eventName, listener);
-    }
-
-    static off(eventName: string, listener: Listener): void {
-
-        Evented._off(undefined, eventName, listener);
-    }
-
-    static listensTo(eventName: string): boolean {
-
-        return Evented._listensTo(undefined, eventName);
-    }
-
-    private static _on<T>(instance: Evented | undefined, eventName: string, listener: Listener<T>): () => void {
-
-        let listeners: { [eventName: string]: Array<Listener>};
-
-        if (instance) {
-            listeners = instance.listeners;
-        } else {
-            listeners = Evented.globalListeners;
+    if (listeners[eventName] instanceof Array) {
+      eventListeners = listeners[eventName];
+      for (i = 0, length = eventListeners.length; i < length; i++) {
+        if (eventListeners[i] === listener) {
+          eventListeners.splice(i, 1);
+          break;
         }
-
-        if (listeners[eventName] === undefined) {
-            listeners[eventName] = [];
-        }
-
-        listeners[eventName].push(listener);
-
-        return () => {
-          Evented._off(instance, eventName, listener);
-        };
-
+      }
     }
 
-    private static _off(instance: Evented | undefined, eventName: string, listener: Listener): void {
+  }
 
-        let listeners: { [eventName: string]: Array<Listener>},
-            eventListeners: Listener[],
-            i: number,
-            length: number;
+  private static _fire<T>(instance: Evented | undefined, eventName: string, eventArgs?: T, eventTarget?: Object): void {
 
+    let eventListeners: Listener[],
+      i: number,
+      length: number,
+      listeners: ListenerDictionary,
+      thisArg: Evented | typeof Evented;
 
-        if (instance) {
-            listeners = instance.listeners;
-        } else {
-            listeners = Evented.globalListeners;
-        }
-
-        if (listeners[eventName] instanceof Array) {
-            eventListeners = listeners[eventName];
-            i = 0;
-            length = eventListeners.length;
-            for (; i < length; i++) {
-                if (eventListeners[i] === listener) {
-                    eventListeners.splice(i, 1);
-                    break;
-                }
-            }
-        }
-
+    if (instance) {
+      listeners = instance.listeners;
+      thisArg = instance;
+    } else {
+      listeners = Evented.globalListeners;
+      thisArg = Evented;
     }
 
-    private static _fire<T>(instance: Evented | undefined, eventName: string, eventArgs?: T, eventTarget?: Object): void {
+    if (listeners[eventName] instanceof Array) {
+      eventListeners = listeners[eventName];
+      for (i = 0, length = eventListeners.length; i < length; i++) {
+        eventListeners[i].call(thisArg, new Event(eventName, eventArgs, eventTarget));
+      }
+    }
+  }
 
-        let listeners: { [eventName: string]: Array<Listener>},
-            eventListeners: Listener[],
-            i: number,
-            length: number,
-            thisArg: any;
+  private static _listensTo(instance: Evented | undefined, eventName: string): boolean {
 
-        if (instance) {
-            listeners = instance.listeners;
-            thisArg = instance;
-        } else {
-            listeners = Evented.globalListeners;
-            thisArg = Evented;
-        }
+    let listeners: ListenerDictionary;
 
-        if (listeners[eventName] instanceof Array) {
-            eventListeners = listeners[eventName];
-            length = eventListeners.length;
-            i = 0;
-
-            for (; i < length; i++) {
-                eventListeners[i].call(thisArg, new Event(eventName, eventArgs, eventTarget));
-            }
-        }
+    if (instance) {
+      listeners = instance.listeners;
+    } else {
+      listeners = Evented.globalListeners;
     }
 
-    private static _listensTo(instance: Evented | undefined, eventName: string): boolean {
-
-        let listeners: { [eventName: string]: Array<Listener>};
-
-        if (instance) {
-            listeners = instance.listeners;
-        } else {
-            listeners = Evented.globalListeners;
-        }
-
-        return (listeners[eventName] instanceof Array && listeners[eventName].length > 0);
-    }
+    return (listeners[eventName] instanceof Array && listeners[eventName].length > 0);
+  }
 }
 
 export class Event<T = any> {
-    private _name: string;
-    private _args: T | undefined;
-    private _target: Object | undefined;
+  private _name: string;
+  private _args: T | undefined;
+  private _target: Object | undefined;
 
-    constructor(name: string, args?: T, target?: Object) {
-        this._name = name;
-        this._args = args;
-        this._target = target;
-    }
+  constructor(name: string, args?: T, target?: Object) {
+    this._name = name;
+    this._args = args;
+    this._target = target;
+  }
 
-    get name(): string {
-        return this._name;
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    get args(): T | undefined {
-        return this._args;
-    }
+  get args(): T | undefined {
+    return this._args;
+  }
 
-    get target(): Object | undefined {
-        return this._target;
-    }
+  get target(): Object | undefined {
+    return this._target;
+  }
 }
 
 export type Listener<T = any> = (event: Event<T>) => void;
+type ListenerDictionary = { [eventName: string]: Listener[] };
